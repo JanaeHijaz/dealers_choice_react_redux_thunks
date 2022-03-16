7897f32f3b8556dbd6721d4fddd06651b2bd31aa
Janae Edwards <edwards.janae@gmail.com>
2022-03-16 14:13:10 -0400

    Main component set up with React

--- src/index.js
+++ src/index.js
@@ -1,4 +1,53 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import store from './store';
+import axios from 'axios';
 
-render(<hr />, document.querySelector('#root'));
+
+// set up Main component and methods
+
+class Main extends React.Component {
+    constructor() {
+     super()
+     this.state = {
+         habits: []
+     };
+
+    }
+
+    async componentDidMount(){
+        const response = await axios.get('/api/habits');
+        const habits = response.data;
+        this.setState({habits: habits});
+    }
+
+    // create method
+    // delete method
+    // update method
+
+    render() {
+        return (
+            <div id='main'>
+                <h1>Self Care Habit Tracker</h1>
+                <button> Add New Habit</button>
+                <div>
+                    {this.state.habits.map(habit => {
+                        return (
+                            <div key={habit.id}>
+                                <div>
+                                    {habit.habitName}
+                                </div>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
+    }
+
+}
+
+
+render( <Main />, 
+document.querySelector('#root'));
